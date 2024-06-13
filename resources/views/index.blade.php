4288b7d4014590f47756d8db2cf93e4cd66a8aa4
unknown <sulthanrafialfarisi@smkwikrama.sch.id>
2024-06-13 19:54:44 +0700

    laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <nav>
+        <h2 class="logo">Portofo<span>lio</span></h2>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#service">Service</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+
+    <div class="main-cont">
+        </div>
+        <div class="content">
+            <h4>Hello, My Name Is </h4>
+            <h1>Sulthan Rafi <span>Alfarisi</span></h1>
+            <h3>I'am a Content Creator</h3>
+            <a href="#about"><i class="fa-regular fa-user"> More About Me</i></a>
+        </div>
+    </div>
+
+    <section class="about" id="about">
+        <div class="about-me">
+            <img src="img/img.png">
+            <div class="about-text">
+                <h2>About <span>Me</span></h2>
+                <p>Perkenalkan saya Sulthan Rafi Alfarisi biasa di panggil Sultan, Hobi saya ada banyak, apa aja bisa
+                    saya lakukan.</p>
+                <p>Saya Tinggal di JL Muara Indah 1 RW3/RT1 Sindangrasa Bogor Timur, asal sekolah saya di Pesantren
+                    Terpadu Al Kahfi Spn Lido</p>
+                <p>Kebiasaan sehari hari biasanya saya melakukan kegiatan yang menyibukkan saya sendiri dengan begitu
+                    gitu aja </p>
+                <a href="#contact" class="button">Contact Me</a>
+            </div>
+        </div>
+    </section>
+
+<section id="portofolio">
+    <h1 class="headings">Portofo<span>lio</span></h1>
+    <div class="gallery">
+        <img src="img/screenshot 2024-06-13 174734.png" alt="">
+        <img src="img/Biru Putih Estetik Modern Sertifikat Dokumen A4 .png" alt="">
+        <img src="img/Black Gold Classy Sertifikat Keanggotaan Certificate.png" alt="">
+        <img src="img/Black Gold Classy Sertifikat Keanggotaan Certificate.png" alt="">
+        <img src="img/Biru Putih Estetik Modern Sertifikat Dokumen A4 .png" alt="">
+        <img src="img/screenshot 2024-06-13 174734.png" alt="">
+    </div>
+
+</section>
+
+<section id="services">
+    <h1 class="headings">Brand<span>ing</span></h1>
+    <div class="row">
+        <div class="box">
+            
+            <h1 class="header">Pengusaha</h1>
+            <p>Pengusaha adalah seseorang, kelompok, ataupun lembaga yang melakukan kegiatan jual, beli, atau sewa sesuatu. Banyak hal yang bisa dikategorikan dalam pengusaha, contohnya seperti produsen sepatu, perternakan ayam, eksport-import bahan baku atau sebuah produk, menjual jasa, dan lain-lain sebagainya.</p>
+        </div>
+        <div class="box">
+           
+            <h1 class="header">Content <br> Creator</h1>
+            <p>Content Creator. Mereka adalah individu kreatif yang menggunakan keterampilan mereka untuk menghasilkan konten yang menarik dan inspiratif di berbagai platform online. Dari video YouTube hingga media sosial seperti Instagram, Twitter, dan TikTok, Content Creator membentuk dan memengaruhi cara kita berinteraksi dengan dunia digital</p>
+        </div>
+        <div class="box">
+            
+            <h1 class="header">Atlet</h1>
+            <p>Olahraga bukan hanya sekadar aktivitas fisik, tetapi juga panggung bagi para atlet untuk menunjukkan dedikasi dan keterampilan mereka. Seorang atlet tidak hanya berkompetisi untuk memenangkan pertandingan, tetapi juga mewakili semangat sportivitas dan keunggulan dalam disiplin olahraga mereka</p>
+        </div>
+    </div>
+
+</section>
+
+    <section class="contact-me" id="contact">
+        <div class="contact-text">
+            <h2>Contact <span>Me</span></h2>
+            <div class="contact-info">
+                <div class="contact-box">
+                    <div class="contact">
+                        <h4>Address</h4>
+                        <p>Sulthan Rafi Alfarisi</p>
+                    </div>
+                    <div class="contact">
+                        <h4>Email</h4>
+                        <p>sulthanrafialfarisi@smkwikrama.sch.id</p>
+                    </div>
+                    <div class="contact">
+                        <h4>Telp</h4>
+                        <p>0812310077</p>
+                    </div>
+                </div>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.074859136164!2d106.83370567403134!3d-6.6376257648851125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c8bcbeb33823%3A0x525c9c250f6eaff3!2sJl.%20Muara%20Indah%20I%2C%20RT.01%2FRW.03%2C%20Sindangrasa%2C%20Kec.%20Bogor%20Tim.%2C%20Kota%20Bogor%2C%20Jawa%20Barat!5e0!3m2!1sen!2sid!4v1700553801575!5m2!1sen!2sid"
+                    width="1000px" height="420px" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+
+    </section>
+
+
+</body>
+
+</html>
